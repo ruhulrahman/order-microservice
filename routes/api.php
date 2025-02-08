fd4bccc9f8873fcf2fd88039c0935b7e762b7d25
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2025-02-08 22:35:14 +0600

    updated

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,7 @@
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('orders', OrderController::class);
+Route::middleware('auth.memcached')->group(function () {
+    // Route::get('/orders', [OrderController::class, 'index']);
+    Route::apiResource('orders', OrderController::class);
+});
